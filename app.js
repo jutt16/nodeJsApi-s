7259e0d22fe15f2eaaf3d79f0e102eb957b0440d
jutt16 <shahidfaqeer02@gmail.com>
2024-08-08 16:44:56 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const products_routes = require("./routes/products");
+
+app.get("/",(req, res) => {
+    res.send("Hi, I am live");
+});
+
+app.use("/api/products",products_routes);
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log('${PORT} Yes I am connected');
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
